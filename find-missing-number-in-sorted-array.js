7f277f487b6aac7d63927dd7651750d242ed1bcd
MattInReality <mattnorth84@gmail.com>
2023-02-24 21:22:07 +0000

    The algorithm was failing because it was missing the final iteration required to get the result if the missing number was the middle number.

--- find-missing-number-in-sorted-array.js
+++ find-missing-number-in-sorted-array.js
@@ -1,10 +1,16 @@
+/*
+
+* We could improve this function by taking in an extra parameter telling it what number we are starting from.
+* */
 export function findMissingNumberInSortedArray(arr) {
+	// Quick returns on extremes
 	if(arr[0] !== 1) {
 		return
 	}
 	if(arr[arr.length - 1] !== arr.length + 1) {
 		return arr.length + 1
 	}
+	// Looping the array checking each element against the next to ensure it's only +1.
 	// O(n)
 	// let current = firstNum
 	// for (let i = 0; i < arr.length; i++) {
@@ -15,9 +21,10 @@ export function findMissingNumberInSortedArray(arr) {
 	// 	}
 	// }
 	// O(Log n)
+	// A binary search
 	let start = 0
 	let end = arr.length - 1
-	while (start < end) {
+	while (start <= end) {
 		let mid = Math.floor(((start + end)) / 2)
 		if((arr[mid] - mid) === 1 && (arr[mid +1]) - (  mid +1) === 2) {
 			return arr[mid] + 1
